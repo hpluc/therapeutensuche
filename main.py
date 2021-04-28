f21b588e3a82f57455a70b1494f3a6711a8c14e2
Merlin <dreger1997@yahoo.de>
2021-04-29 00:08:26 +0200

    Aufrufen der 100. ersten Therapeuten geht ToDo: 1. Herausnehmen der Telefonischen Srechzeit 2. Dateistruktur definieren und speichern der tel. Erreichbarkeit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from requests_html import HTMLSession
+import re
 from urllib.parse import urljoin
 
 def GetTherapeutenJason (Postleitzahl, Umkreis):
@@ -30,54 +31,33 @@ def GetTherapeutenJason (Postleitzahl, Umkreis):
 def getAerzte():
     url ="https://arztsuchehessen.de/arztsuche/arztsuche.php?page=suche&fachrichtung=--alle--&haus_facharzt=egal&fachrichtung_psycho=479&plz=64293&ort=Darmstadt&entfernung=5&action%5BSucheStarten%5D=&name=--alle--&vorname=--alle--&geschlecht=egal&status=--alle--&genehmigung=--alle--&zusatzbezeichnung=--alle--&testungaufSARSCoV2=--alle--&fremdsprache=--alle--&sz_von_sel=&sz_bis_sel="
 
-def get_all_forms(url):
-    """Returns all form tags found on a web page's `url` """
-    # GET request
-    res = session.get(url)
-    # for javascript driven website
-    # res.html.render()
-    soup = BeautifulSoup(res.html.html, "html.parser")
-    return soup.find_all("form")
-
-def get_form_details(form):
-    """Returns the HTML details of a form,
-    including action, method and list of form controls (inputs, etc)"""
-    details = {}
-    # get the form action (requested URL)
-    action = form.attrs.get("action").lower()
-    # get the form method (POST, GET, DELETE, etc)
-    # if not specified, GET is the default in HTML
-    method = form.attrs.get("method", "get").lower()
-    # get all form inputs
-    inputs = []
-    for input_tag in form.find_all("input"):
-        # get type of input form control
-        input_type = input_tag.attrs.get("type", "text")
-        # get name attribute
-        input_name = input_tag.attrs.get("name")
-        # get the default value of that input tag
-        input_value =input_tag.attrs.get("value", "")
-        # add everything to that list
-        inputs.append({"type": input_type, "name": input_name, "value": input_value})
-    # put everything to the resulting dictionary
-    details["action"] = action
-    details["method"] = method
-    details["inputs"] = inputs
-    return details
-
-
-
-print(GetTherapeutenJason(64293,10))
-
 # initialize an HTTP session
 session = HTMLSession()
 res = session.get("https://arztsuchehessen.de/arztsuche/arztsuche.php?page=erweiterteSuche&switch=umkreissuche")
 res = session.get("https://arztsuchehessen.de/arztsuche/arztsuche.php?page=suche&fachrichtung=44&haus_facharzt=egal&fachrichtung_psycho=--alle--&plz=64283&ort=Darmstadt&entfernung=10&action%5BSucheStarten%5D=&name=--alle--&vorname=--alle--&geschlecht=egal&status=--alle--&genehmigung=--alle--&zusatzbezeichnung=--alle--&testungaufSARSCoV2=--alle--&fremdsprache=--alle--&sz_von_sel=&sz_bis_sel=")
+res= session.get("https://arztsuchehessen.de/arztsuche/arztsuche.php?page=ergebnisliste&rpp=100")
 soup = BeautifulSoup(res.content, "html.parser")
-#print(soup.text)
 table = soup.find('table', {"class": "ergebnisliste"})  # isolate the table
-print(table.getText)
-#td_list = []  # initialize a list to store the td's we want
-#for tr in table.find_all('tr bgcolor="#fbfbfb"'):  # loop through each tr in the table
-#    td_list.append(tr.find_all('td')[0])  # append the 2nd td to the td_list
-#print(td_list)
+td_list = []  # initialize a list to store the td's we want
+links = []
+i=int(0)
+for td in table.find_all('td'):  # loop through each tr in the table
+    i = i + 1
+    if i == 1:
+        td_list.append(td)
+        #print(td)
+        link = re.findall('arzt.*[0-9]"',str(td))
+        links.append(link[0])
+        #print (link)
+    if i == 3:
+        i=0
+for link in links:
+    link = link[:-1]
+    link = link.replace("amp;","")
+    link = "https://arztsuchehessen.de/arztsuche/" + link
+    print (link)
+    res = session.get(link)
+    soup = BeautifulSoup(res.content, "html.parser")
+    data_all = soup.find_all("div", class_="Sprechzeit")
+    #Sprechzeit = re.findall('Telefonische Erreichbarkeit .* Sprechstunde', str(soup.text))
+    print (data_all)
